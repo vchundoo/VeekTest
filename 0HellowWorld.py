34c4523167a569ef69d8f838b52f659a059c330d
vchundoo <164516651+vchundoo@users.noreply.github.com>
2024-03-22 14:11:35 +0000

    Create 0HellowWorld.py

---
+++ 0HellowWorld.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+age = 20
+price = 19.95
+print (age)
+first_name = "Veek"
+is_online = False
+
+Name = input("What is your name? ")
+print ("Your name is ", Name)
+
+birth_year = input("Enter your birth year ")
+age = 2024 - int(birth_year)
+print("you are are",age, "years old")
+print(f"you are are {age} years old")
